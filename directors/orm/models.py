0265e12930c29094f9b5311e15b383349b9e0876
Jon <jon@rivingtondigital.com>
2020-03-11 18:35:29 -0700

    add alembic migrations

--- directors/orm/models.py
+++ directors/orm/models.py
@@ -1,5 +1,5 @@
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, Integer, String, UniqueConstraint
+from sqlalchemy import Column, Integer, String, Boolean, UniqueConstraint
 from directors.orm import engine, session_scope
 
 
@@ -9,13 +9,14 @@ Base = declarative_base()
 class Company(Base):
     __tablename__ = 'company'
     __table_args__ = (
-        UniqueConstraint('symbol', 'market'),
+        UniqueConstraint('symbol', 'market', name='unq_symbol_market'),
     )
 
     id = Column(Integer, primary_key=True)
-    name = Column(String(255))
-    symbol = Column(String(8))
-    market = Column(String(20))
+    name = Column(String(255), nullable=False)
+    symbol = Column(String(8), nullable=False)
+    market = Column(String(20), nullable=False)
+    deleted = Column(Boolean(), default=False)
 
     def __repr__(self):
         return "<Company: {}>".format(self.symbol)
@@ -24,7 +25,8 @@ class Company(Base):
     def batch_update(cls, batch, market=None):
         batch = set(batch)
         with session_scope() as sess:
-            table_data = set(sess.query(Company).filter(Company.market == market)
+            table_data = set(sess.query(Company)
+                             .filter(Company.market == market)
                              .values('name', 'symbol', 'market'))
 
         new_listings = batch - table_data
@@ -39,11 +41,11 @@ class Company(Base):
             for listing in deleted_listings:
                 sess.query(Company).filter_by(**listing._asdict()).delete()
 
-
         return {
             'new': new_listings,
             'deleted': deleted_listings,
             'unchanged': unchanged
         }
 
-Base.metadata.create_all(engine)
+
+# Base.metadata.create_all(engine)
